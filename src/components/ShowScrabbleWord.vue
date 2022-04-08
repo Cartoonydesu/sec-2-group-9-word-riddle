f58e97a8dc23bcc57bdee62311d194f78726995b
Cartoonydesu <toon.pi45@gmail.com>
2022-04-08 14:49:49 +0700

    Do Scrabble game

--- src/components/ShowScrabbleWord.vue
+++ src/components/ShowScrabbleWord.vue
@@ -1,30 +1,82 @@
 <script setup>
-    // defineEmits(['reset'])
-    // defineProps({
-    //     words:{
-    //         type: Object,
-    //         require: true
-    //     },
-    //     desc:{
-    //         type: Object,
-    //         require: true
-    //     }
-    // })
+    import { ref, computed } from 'vue'
+    defineEmits(['reset'])
 
-    //--------- PART - Letter -------------
-    // //ตัวอักษรทั้งหมด เก็บเป็น array
-    // const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
-    //                 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    // //ชีวิต เริ่มที่ 10 
-    // const lives = ref(10)
+    const props = defineProps({
+        words:{
+            type: Object,
+            require: true
+        },
+        arrayOfWord:{
+            type: Array,
+            require: true
+        },
+        swapArrayOfWord:{
+            type: Array,
+            require: true
+        }
 
-    // //function ในการอ่ะ รีๆๆๆๆๆๆๆๆ
-    // const playAgain = () => {
-    // location.reload()
-    // }  
+    })
+
+    console.log(props.words)
+    console.log(props.arrayOfWord)   
+
+    // PART game --------------------
+    const inputWord = ref('')
+    const matchWord = computed(() => {
+        return props.words.word === inputWord.value
+    })
+ 
+    //PART countdown
+
+    const timeLeft = ref(60);
+    const timeOut =ref(false)
+    const showHint = ref(false)
+
+    function countDown(){  
+        if(timeLeft.value==20) showHint.value = true
+        if(timeLeft.value<= 0) timeOut.value = true
+        else if(!matchWord.value) timeLeft.value --
+
+    }
+
+    setInterval(countDown,1000)
+
+    //PART hint
+    const showHintBox = ref(false)
+
 </script>
  
 <template>
+
+
+
+    <div>
+        <h3>Scrabble the word : </h3>
+        <h2>
+            <span v-for="letter in swapArrayOfWord">
+                {{ letter }}
+            </span>
+        </h2>
+    </div>
+
+    <div>
+        <span v-show="!timeOut">Time left : {{ timeLeft }} seconds</span>
+        <h4 v-show="timeOut">Time out ! Game over ! This answer is {{ props.words.word }}</h4>
+        <h4 v-show="matchWord">Correct !!</h4>
+    </div>
+    
+    <br>
+
+    <div>
+        <input type="text" placeholder="Guess the word!" v-model="inputWord" :disabled="matchWord"/>
+    </div>
+
+    <div>
+        <button v-show="showHint" @click="showHintBox = true">Hint</button>
+        <span v-show="showHintBox">{{ words.hint }}</span>
+        <button @click="($emit('reset'))">Reset</button>
+    </div>
 
 </template>
  
--- src/views/ScrabbleWord.vue
+++ src/views/ScrabbleWord.vue
@@ -1,18 +1,51 @@
 <script setup>
     import { useWords } from '../stores/listAllWords.js'
+    import { ref, onMounted, } from 'vue'
     import ShowScrabbleWord from '../components/ShowScrabbleWord.vue'
+ 
     const store = useWords()
-//     const randomWord = () => {
-//     return store[Math.floor(Math.random() * store.length)]
-//     }
+    const randomWord = ref({})
+    const arrayOfWord = ref([])
+    const swapArrayOfWord = ref([])
+    onMounted(async () => {
+        await store.getWords()
+        randomWord.value = store.listWords[Math.floor(Math.random() * store.listWords.length)]
+        arrayOfWord.value = randomWord.value.word.split('')
+        swapArrayOfWord.value = randomWord.value.word.split('')
+        console.log(swapArrayOfWord.value)
+        swapArrayElements(swapArrayOfWord.value)
+        console.log(swapArrayOfWord.value)
+    })
 
-//     //function ในการอ่ะ รีๆๆๆๆๆๆๆๆ
-//     const playAgain = () => { location.reload() }
+
+
+    const swapArrayElements = (array) => {
+        let arrayLength = array.length
+        let x, y
+        let i = 0
+        while(i < arrayLength){
+            x = Math.floor(Math.random()*arrayLength)
+            y = Math.floor(Math.random()*arrayLength)
+            // console.log(x+' '+y)
+            array.splice(y, 1, array.splice(x, 1, array[y])[0]);
+            // console.log(array)
+            i++
+        }
+    }
+            
+    const reset = () => {location.reload()}
+
 </script>
  
 <template>
-    <h2>Scabble Word (views)</h2>
-    <!-- <Scabble :words="store"/> -->
+    <h2>Scabble Word</h2>
+
+    <ShowScrabbleWord 
+        :words="randomWord"
+        :arrayOfWord="arrayOfWord"
+        :swapArrayOfWord="swapArrayOfWord"
+        @reset="reset"/>
+
 </template>
  
 <style>
